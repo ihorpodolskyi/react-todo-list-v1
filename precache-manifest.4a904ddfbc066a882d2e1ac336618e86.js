702b244ed58e1930fc5ccb907027f5a5126a6669
Igor Podolskiy <igorpodolskiy82@gmail.com>
2021-03-28 05:15:59 +0300

    Updates

---
+++ precache-manifest.4a904ddfbc066a882d2e1ac336618e86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647b633f09983cda8507b40113348d33",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c7b0287610ab09868a86",
+    "url": "/react-todo-list/static/css/main.96c2daa1.chunk.css"
+  },
+  {
+    "revision": "dba419e4885715f8ecc3",
+    "url": "/react-todo-list/static/js/2.081e718a.chunk.js"
+  },
+  {
+    "revision": "c7b0287610ab09868a86",
+    "url": "/react-todo-list/static/js/main.0e7021bc.chunk.js"
+  },
+  {
+    "revision": "70b590f0f67ba174039c",
+    "url": "/react-todo-list/static/js/runtime-main.48d04789.js"
+  },
+  {
+    "revision": "5dd65911000f6889279dfcd4de32aa0b",
+    "url": "/react-todo-list/static/media/clear.5dd65911.png"
+  },
+  {
+    "revision": "e173066f49e44114bc90badd9878e67e",
+    "url": "/react-todo-list/static/media/completed.e173066f.png"
+  },
+  {
+    "revision": "40b3772ed5b54e2d569ef68d4384d99b",
+    "url": "/react-todo-list/static/media/edit.40b3772e.png"
+  }
+]);
